5d13aced2cc584c48fc3a33138a90af96c4ba68a
Josip Igrec <josip@igr.ec>
2024-08-10 19:02:02 +0200

    Update twitter downloader params

--- crates/app-downloader/src/downloaders/twitter.rs
+++ crates/app-downloader/src/downloaders/twitter.rs
@@ -218,43 +218,49 @@ fn get_tweet_data(tweet_id: &str) -> Result<TweetData, String> {
     let query_params = {
         let graphql_variables = json!({
             "tweetId": tweet_id,
-            "referrer": "home",
-            "with_rux_injections": false,
-            "includePromotedContent": false,
             "withCommunity": false,
-            "withQuickPromoteEligibilityTweetFields": false,
-            "withBirdwatchNotes": false,
+            "includePromotedContent": false,
             "withVoice": false,
-            "withV2Timeline": false,
         })
         .to_string();
         let graphql_features = json!({
-            "rweb_lists_timeline_redesign_enabled": true,
-            "responsive_web_graphql_exclude_directive_enabled": true,
-            "verified_phone_label_enabled": true,
             "creator_subscriptions_tweet_preview_api_enabled": true,
-            "responsive_web_graphql_timeline_navigation_enabled": true,
-            "responsive_web_graphql_skip_user_profile_image_extensions_enabled": false,
-            "tweetypie_unmention_optimization_enabled": true,
+            "communities_web_enable_tweet_community_results_fetch": true,
+            "c9s_tweet_anatomy_moderator_badge_enabled": true,
+            "articles_preview_enabled": true,
             "responsive_web_edit_tweet_api_enabled": true,
             "graphql_is_translatable_rweb_tweet_is_translatable_enabled": true,
             "view_counts_everywhere_api_enabled": true,
             "longform_notetweets_consumption_enabled": true,
-            "responsive_web_twitter_article_tweet_consumption_enabled": false,
+            "responsive_web_twitter_article_tweet_consumption_enabled": true,
             "tweet_awards_web_tipping_enabled": false,
+            "creator_subscriptions_quote_tweet_preview_enabled": false,
             "freedom_of_speech_not_reach_fetch_enabled": true,
             "standardized_nudges_misinfo": true,
             "tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled": true,
+            "rweb_video_timestamps_enabled": true,
             "longform_notetweets_rich_text_read_enabled": true,
             "longform_notetweets_inline_media_enabled": true,
-            "responsive_web_media_download_video_enabled": false,
+            "rweb_tipjar_consumption_enabled": true,
+            "responsive_web_graphql_exclude_directive_enabled": true,
+            "verified_phone_label_enabled": false,
+            "responsive_web_graphql_skip_user_profile_image_extensions_enabled": false,
+            "responsive_web_graphql_timeline_navigation_enabled": true,
             "responsive_web_enhance_cards_enabled": false,
+        })
+        .to_string();
+        let field_toggles = json!({
+            "withArticleRichContentState": true,
+            "withArticlePlainText": false,
+            "withGrokAnalyze": false,
+            "withDisallowedReplyControls": false,
         })
         .to_string();
 
         &[
             ("variables", graphql_variables),
             ("features", graphql_features),
+            ("fieldToggles", field_toggles),
         ]
     };
 
